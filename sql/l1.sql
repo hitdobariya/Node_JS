60d2e9ff678ad2aeb3fff401a837a06d5aad12c2
hitdobariya <hitdobariya001@gmail.com>
2024-07-01 08:56:51 +0530

    27/06/2024

---
+++ sql/l1.sql
@@ -0,0 +1,49 @@
+> enter mysql_query
+-- \sql 
+
+
+> connect mysql
+-- \connect root@Localhost:3306 
+--admin@123 -password
+
+
+> show database
+-- show databases;  
+
+
+> create database
+-- create database name; 
+
+
+> enter database
+-- use name;   
+
+
+> delete database
+-- drop database name; 
+
+
+> show table
+-- show tables; 
+
+
+> create table
+-- create table name(
+--    username varchar(value) , 
+--    lastname varchar(value) , 
+--    gender varchar(6) , 
+--    age int , 
+--    email varchar(25) , 
+--    mobile int(10)
+-- ); 
+
+
+> show table in row and collums
+-- describe name; 
+      or 
+-- desc name; 
+
+>delete table
+--drop name;
+
+
